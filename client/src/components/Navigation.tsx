d281a694984f63b36e9fdf37ee267d5fcedf3ec8
Michael Mosquera <michael2000ny@gmail.com>
2025-02-26 14:35:11 -0500

    fixing bttns in navabr

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -27,37 +27,33 @@ const Navigation = () => {
     <nav className="navbar navbar-expand-lg custom-navbar">
       <div className="container">
         <Link className="navbar-brand" to="/">Invyt</Link>
-        <div id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/create-event">Create Event</Link>
-                </li>
-                <li className="nav-item dropdown" ref={dropdownRef}>
-                  <button 
-                    className="btn dropdown-toggle profile-dropdown" 
-                    onClick={() => setDropdownOpen(!dropdownOpen)}
-                  >
-                    <span className="desktop-name">{user.name}</span>
-                    <span className="mobile-initials">{user.name.charAt(0)}</span>
-                  </button>
-                  <ul className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}>
-                    <li>
-                      <Link className="dropdown-item" to="/profile">My Profile</Link>
-                    </li>
-                    <li>
-                      <button className="dropdown-item logoutText" onClick={logout}>Logout</button>
-                    </li>
-                  </ul>
-                </li>
-              </>
-            ) : (
-              <li className="nav-item">
-                <a href="http://localhost:3001/auth/google" className="btn loginButton">Login</a>
+        <div id="navbarNav" className="d-flex align-items-center">
+          {user ? (
+            <>
+              <Link className="nav-link create-event-button" to="/create-event">
+                Create Event
+              </Link>
+              <li className="nav-item dropdown" ref={dropdownRef}>
+                <button 
+                  className="btn dropdown-toggle profile-dropdown" 
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                >
+                  <span className="desktop-name">{user.name}</span>
+                  <span className="mobile-initials">{user.name.charAt(0)}</span>
+                </button>
+                <ul className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}>
+                  <li>
+                    <Link className="dropdown-item" to="/profile">My Profile</Link>
+                  </li>
+                  <li>
+                    <button className="dropdown-item logoutText" onClick={logout}>Logout</button>
+                  </li>
+                </ul>
               </li>
-            )}
-          </ul>
+            </>
+          ) : (
+            <a href="http://localhost:3001/auth/google" className="btn loginButton">Login</a>
+          )}
         </div>
       </div>
     </nav>
